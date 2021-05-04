2e2354cfa35a1726b0491fb5b23520cda37fd921
Gabriel Dinner-David <82682503+gabydinnerdavid@users.noreply.github.com>
2021-05-04 09:09:17 -0400

    added crab king imagery and got timing working. Commented to tell me what I need to do next.

--- pyrythms.py
+++ pyrythms.py
@@ -1,14 +1,22 @@
 import pygame as pg
 
+
 class Song:
-    def __init__(self, keys = None, reps = 0):
+    def __init__(self, keys=None, reps=1):
         if keys == None:
             keys = []
         self.keys = keys
         self.reps = reps
 
-    def addKey(self, key):
+    def add_key(self, key):
         self.keys.append(key)
 
-    def correctKey(self, spot):
+    def correct_key(self, spot):
         return f'pg.K_{self.keys[spot]}'
+
+    def with_reps(self):
+        self.keys *= self.reps
+
+    ''' 
+    add ability for sequence of keys denotion and held key denotion
+    '''
